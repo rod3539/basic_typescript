86cab819bcdec12cf4404070f7309744146f15d2
younghun_yoon <younghun_yoon@tmax.co.kr>
2023-06-19 17:28:50 +0900

    Types

---
+++ Types/variables.js
@@ -0,0 +1,5 @@
+var movieTitle = 'Amadeus';
+movieTitle = 'Arrival';
+movieTitle.toUpperCase();
+var myNumber = 10;
+var myBoolean = true;
---
+++ Types/variables.ts
@@ -0,0 +1,7 @@
+let movieTitle: string = 'Amadeus';
+movieTitle = 'Arrival';
+movieTitle.toUpperCase();
+
+let myNumber: number = 10;
+
+let myBoolean: boolean = true;
